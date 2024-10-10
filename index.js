1175182d32cacbc70d8e9acfad8639c36dd81c8e
cl0wder <cl0wder@icloud.com>
2024-10-11 00:44:19 +0200

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+const changeBackgroundButton = document.getElementById('chang-background');
+
+changeBackgroundButton.addEventListener('click', () => {
+    const body = document.body;
+    const randomColor = getRandomColor();
+    body.style.backgroundColor = randomColor;
+});
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const testButton = document.getElementById('test');
+testButton.addEventListener('click', () => {
+    alert('this is a js test');
+    const body = document.body;
+    const randomColor = getRandomColor();
+    body.style.backgroundColor = randomColor;
+})
